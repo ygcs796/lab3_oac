c4dc1c18ef5e6b928e406d484812638a2598ca3e
mzckawa <5689.melzion.cmr@gmail.com>
2026-03-26 14:43:06 -0300

    Add button FSM module for button rising edge detection

---
+++ btn_fsm.sv
@@ -0,0 +1,20 @@
+module button_fsm (
+    input  logic       clk,    
+    input  logic       rst_n,  
+    input  logic       btn,    
+    output logic       btn_rise   
+);
+
+logic btn_active; 
+logic btn_prev;  
+logic btn_rise;    
+
+assign btn_active = ~btn;
+assign btn_rise   = btn_active & ~btn_prev;  
+
+always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) btn_prev <= 1'b0;
+    else        btn_prev <= btn_active;
+end
+
+endmodule
