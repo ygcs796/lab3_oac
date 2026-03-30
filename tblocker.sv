2727cbdd45c48984576af87f9a0a13c925a30972
mzckawa <5689.melzion.cmr@gmail.com>
2026-03-30 16:41:35 -0300

    Correções no testbench

--- tblocker.sv
+++ tblocker.sv
@@ -19,9 +19,9 @@ module tb_locker;
     // tarefa para pressionar o botao mais facil
     task press_btn(input logic [3:0] press_val);
         begin
-            btn = press_val;      // joga 0 no botao(apertado)
+            btn = press_val;      // joga 1 no botao(apertado)
             #20;                  //espera
-            btn = 4'b1111;        // volta pra 1
+            btn = 4'b0000;        // volta pra 0
             #20;                  // espera mais um pouco
         end
     endtask
@@ -30,7 +30,7 @@ module tb_locker;
         $monitor("------------------------");
         clk   = 0;
         rst_n = 0;
-        btn   = 4'b1111; // nenhum botao pressionado
+        btn   = 4'b0000; // nenhum botao pressionado
         
         #20;
         rst_n = 1;       //tira do reset
@@ -38,39 +38,40 @@ module tb_locker;
 
         // cenario do ciclo completo
         $monitor("\n--- Teste 1: Senha Correta ---");
-        press_btn(4'b1110); 
-        press_btn(4'b1101); 
-        press_btn(4'b1011); 
-        press_btn(4'b0111); 
-        
+
+        press_btn(4'b0001); 
+        press_btn(4'b0010); 
+        press_btn(4'b0100); 
+        press_btn(4'b1000); 
+
         // Verifica se abriu
         #10;
         if (open) $monitor("SUCESSO: A fechadura abriu!");
         else      $monitor("FALHA: A fechadura nao abriu.");
         
-        //reseta para o proximo testw
+        //reseta para o proximo teste
         #20;
-        rst_n = 0; #20; rst_n = 1; #20;
+        rst_n = 1; #20; rst_n = 0; #20;
 
         // um botao correto e depois 2 botoes ao mesmo tempo
         $monitor("\n--- Teste 2: Dois Botoes Apertados Juntos ---");
-        press_btn(4'b1110); // correto
+        press_btn(4'b0001); // correto
         
-        press_btn(4'b1001); // pressiona dois ao mesmo tempo
+        press_btn(4'b0110); // pressiona dois ao mesmo tempo
         
         // é pra maquina voltar pra init
         #10;
         $monitor("\nFechadura pos-erro duplo: open = %b", open);
 
-       //resetando denovo
+       //resetando de novo
         #20;
-        rst_n = 0; #20; rst_n = 1; #20;
+        rst_n = 1; #20; rst_n = 0; #20;
 
         // um botao correto e um botao errado em seguida
         $monitor("\n--- Teste 3: Botao Errado no Meio da Sequencia ---");
-        press_btn(4'b1110); // aperta o correto
+        press_btn(4'b0001); // aperta o correto
         
-        press_btn(4'b1011); // aperta um botao depois do que deveria
+        press_btn(4'b0100); // aperta um botao depois do que deveria
         
         // é pra maquina voltar a init
         #10;
