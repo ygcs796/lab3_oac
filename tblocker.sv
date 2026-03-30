54bb4ed8fe850b22eee1361a98ad3170ee49a024
mzckawa <5689.melzion.cmr@gmail.com>
2026-03-30 15:38:37 -0300

    Correcoes no tb

--- tblocker.sv
+++ tblocker.sv
@@ -10,7 +10,7 @@ module tb_locker;
     locker dut (
         .clk(clk),
         .rst_n(rst_n),
-        .btn(btn),
+        .btn_geral(btn),
         .open(open)
     );
 
@@ -27,7 +27,7 @@ module tb_locker;
     endtask
 
     initial begin
-        $display("------------------------");
+        $monitor("------------------------");
         clk   = 0;
         rst_n = 0;
         btn   = 4'b1111; // nenhum botao pressionado
@@ -37,7 +37,7 @@ module tb_locker;
         #20;
 
         // cenario do ciclo completo
-        $display("\n--- Teste 1: Senha Correta ---");
+        $monitor("\n--- Teste 1: Senha Correta ---");
         press_btn(4'b1110); 
         press_btn(4'b1101); 
         press_btn(4'b1011); 
@@ -45,40 +45,40 @@ module tb_locker;
         
         // Verifica se abriu
         #10;
-        if (open) $display("SUCESSO: A fechadura abriu!");
-        else      $display("FALHA: A fechadura nao abriu.");
+        if (open) $monitor("SUCESSO: A fechadura abriu!");
+        else      $monitor("FALHA: A fechadura nao abriu.");
         
         //reseta para o proximo testw
         #20;
         rst_n = 0; #20; rst_n = 1; #20;
 
         // um botao correto e depois 2 botoes ao mesmo tempo
-        $display("\n--- Teste 2: Dois Botoes Apertados Juntos ---");
+        $monitor("\n--- Teste 2: Dois Botoes Apertados Juntos ---");
         press_btn(4'b1110); // correto
         
         press_btn(4'b1001); // pressiona dois ao mesmo tempo
         
         // é pra maquina voltar pra init
         #10;
-        $display("\nFechadura pos-erro duplo: open = %b", open);
+        $monitor("\nFechadura pos-erro duplo: open = %b", open);
 
        //resetando denovo
         #20;
         rst_n = 0; #20; rst_n = 1; #20;
 
         // um botao correto e um botao errado em seguida
-        $display("\n--- Teste 3: Botao Errado no Meio da Sequencia ---");
+        $monitor("\n--- Teste 3: Botao Errado no Meio da Sequencia ---");
         press_btn(4'b1110); // aperta o correto
         
         press_btn(4'b1011); // aperta um botao depois do que deveria
         
         // é pra maquina voltar a init
         #10;
-        $display("\nFechadura pos-erro: open = %b", open);
+        $monitor("\nFechadura pos-erro: open = %b", open);
 
         // acabando a simulação
         #50;
-        $display("\n--------------------------------------");
+        $monitor("\n--------------------------------------");
         $finish;
     end
 
